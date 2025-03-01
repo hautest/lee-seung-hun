44ded31e1063295c9f1a5717edd4ff1ee59dcaf2
hautest <dos07008@naver.com>
2025-03-01 21:10:09 +0900

    chore: deploy yml 수정

--- src/lib/component/MarkdownRender.tsx
+++ src/lib/component/MarkdownRender.tsx
@@ -1,18 +1,8 @@
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Text } from "../ui/Text";
-import { getBlogList } from "../notion/getBlogList";
 
 export async function MarkdownRender() {
-  // const { results } = await notion.databases.query({
-  //   database_id: "1a5a463e8303804a99eeffdd738861d2" as string,
-  // });
-
-  // console.log(results);
-  const pageList = await getBlogList();
-
-  console.log(pageList);
-
   return (
     <Markdown
       components={{
