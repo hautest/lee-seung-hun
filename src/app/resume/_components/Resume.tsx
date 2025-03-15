6fa20f5330ad40a666a518d09d404095c62d09d8
hautest <dos07008@naver.com>
2025-03-15 16:09:49 +0900

    feat: 이력서 페이지 스켈레톤 추가

--- src/app/resume/_components/Resume.tsx
+++ src/app/resume/_components/Resume.tsx
@@ -1,16 +1,35 @@
+"use client";
+
+import { Skeleton } from "@/lib/ui/Skeleton";
+import { useState } from "react";
 import { css } from "styled-system/css";
 
-export async function Resume() {
+export function Resume() {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <iframe
-      src="https://docs.google.com/gview?url=https://lee-seung-hun.com/이승훈_이력서.pdf&embedded=true"
-      className={css({
-        w: "full",
-        h: "5800",
-        mobileDown: {
-          h: "2500",
-        },
-      })}
-    ></iframe>
+    <>
+      {isLoading && (
+        <Skeleton
+          css={{
+            h: "5800",
+            mobileDown: {
+              h: "2500",
+            },
+          }}
+        />
+      )}
+      <iframe
+        src="https://docs.google.com/gview?url=https://lee-seung-hun.com/이승훈_이력서.pdf&embedded=true"
+        className={css({
+          w: "full",
+          h: isLoading ? "0" : "5800",
+          mobileDown: {
+            h: isLoading ? "0" : "2500",
+          },
+        })}
+        onLoad={() => setIsLoading(false)}
+      ></iframe>
+    </>
   );
 }
