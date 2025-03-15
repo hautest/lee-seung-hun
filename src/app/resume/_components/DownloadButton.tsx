a218c27bc9c69b506181dc93661d38f1e882d7fb
hautest <dos07008@naver.com>
2025-03-15 16:00:22 +0900

    feat: 이력서 페이지 수정 몇 다운로드 기능 추가

--- src/app/resume/_components/DownloadButton.tsx
+++ src/app/resume/_components/DownloadButton.tsx
@@ -5,8 +5,11 @@ export function DownloadButton() {
     <Button
       variant="solid"
       css={{ colorPalette: "neutral", width: "fit", alignSelf: "center" }}
+      asChild
     >
-      이력서 PDF로 다운로드
+      <a href="/이승훈_이력서.zip" download>
+        이력서 PDF로 다운로드
+      </a>
     </Button>
   );
 }
--- src/app/resume/_components/Resume.tsx
+++ src/app/resume/_components/Resume.tsx
@@ -2,16 +2,15 @@ import { css } from "styled-system/css";
 
 export async function Resume() {
   return (
-    <object
-      data="http://docs.google.com/gview?url=https://lee-seung-hun.com/이승훈_이력서.pdf&embedded=true"
-      type="application/pdf"
+    <iframe
+      src="https://docs.google.com/gview?url=https://lee-seung-hun.com/이승훈_이력서.pdf&embedded=true"
       className={css({
         w: "full",
         h: "5800",
         mobileDown: {
-          h: "auto",
+          h: "2500",
         },
       })}
-    ></object>
+    ></iframe>
   );
 }
