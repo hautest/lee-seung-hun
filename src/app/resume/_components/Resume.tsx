a94b8749168f76c720c3d3aa8ebd88bd6ab9e5b7
hautest <dos07008@naver.com>
2025-03-21 00:34:47 +0900

    feat: 이력서 페이지 iframe 간헐적으로 로딩 안되는 현상 key update로 수정

--- src/app/resume/_components/Resume.tsx
+++ src/app/resume/_components/Resume.tsx
@@ -1,11 +1,37 @@
 "use client";
 
 import { Skeleton } from "@/lib/ui/Skeleton";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { css } from "styled-system/css";
+
+const RESUME_URL =
+  "https://docs.google.com/gview?url=https://lee-seung-hun.com/이승훈_이력서.pdf&embedded=true";
 
 export function Resume() {
   const [isLoading, setIsLoading] = useState(true);
+  const [iframeKey, setIframeKey] = useState(0);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    if (isLoading) {
+      timerRef.current = setTimeout(() => {
+        // iframe이 로딩되지 않았다면, iframeKey를 업데이트하여 다시 시도
+        setIframeKey((prevKey) => prevKey + 1);
+      }, 2000);
+    }
+
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+        timerRef.current = null;
+      }
+    };
+  }, [isLoading, iframeKey]);
+
+  const handleLoad = () => {
+    setIsLoading(false);
+    if (timerRef.current) clearTimeout(timerRef.current);
+  };
 
   return (
     <>
@@ -20,7 +46,8 @@ export function Resume() {
         />
       )}
       <iframe
-        src="https://docs.google.com/gview?url=https://lee-seung-hun.com/이승훈_이력서.pdf&embedded=true"
+        key={iframeKey}
+        src={RESUME_URL}
         className={css({
           w: "full",
           h: isLoading ? "0" : "5800",
@@ -28,7 +55,7 @@ export function Resume() {
             h: isLoading ? "0" : "2500",
           },
         })}
-        onLoad={() => setIsLoading(false)}
+        onLoad={handleLoad}
       ></iframe>
     </>
   );
