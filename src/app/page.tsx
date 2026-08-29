d9f50c8e4e95de39751e5b7e476b821546a0022b
hautest <dos07008@naver.com>
2026-08-30 04:39:20 +0900

    Revert "style:  home mobile 스타일 수정"
    This reverts commit aaf6e0bccdefe0d3d692c5ff1fcfb707a99bdac9.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
         gap: "6",
         paddingTop: "2",
         w: "full",
-        mobile: {
+        mobileDown: {
           position: "initial",
           transform: "translate(0, 0)",
         },
